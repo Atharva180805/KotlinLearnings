7d1e0c8e4a20a62729c701e9458d4adeb4b724e9
huhu <methodsomp@gmail.com>
2025-07-16 18:54:07 +0530

    changed file name to be specific with topic

---
+++ src/LeapYear.kt
@@ -0,0 +1,12 @@
+fun main  ()
+{
+    var year = 2025
+
+    if(year%4==0&&year%100!=0||year%400==0){
+        println("$year is a leap year")
+    }
+    else
+    {
+        println("$year is not a leap year")
+    }
+}
