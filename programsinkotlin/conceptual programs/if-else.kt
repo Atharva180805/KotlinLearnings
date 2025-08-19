7ef2e019c996727726805e3205f5bc4d2c975858
huhu <methodsomp@gmail.com>
2025-08-20 05:23:06 +0530

    changed file name to be specific with topic

---
+++ programsinkotlin/conceptual programs/if-else.kt
@@ -0,0 +1,14 @@
+package `conceptual programs`
+
+fun main()
+{
+   var value = readln().toInt()
+   if (value < 0)
+   {
+     println("The number is negative")
+   }
+    else
+   {
+    println("the value is positive")
+   }
+}
