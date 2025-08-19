1c6675ba163b5af01b23914166165b51a2486633
huhu <methodsomp@gmail.com>
2025-08-19 23:51:33 +0530

    changed file name to be specific with topic

---
+++ programsinkotlin/conceptual programs/nullable datatype.kt
@@ -0,0 +1,9 @@
+package `conceptual programs`
+
+fun main()
+{
+   var hello : String? = null
+    println("the initial value of hello variable with nullable datatype is $hello")
+    hello = "Atharva"
+    println("the updated value of hello variable with nullable datatype is $hello")
+}
