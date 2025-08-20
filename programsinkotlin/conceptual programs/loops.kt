d7a031d60631cad988a0b6567f56eb9c874d6bc0
huhu <methodsomp@gmail.com>
2025-08-20 05:44:07 +0530

    changed file name to be specific with topic

---
+++ programsinkotlin/conceptual programs/loops.kt
@@ -0,0 +1,24 @@
+package `conceptual programs`
+
+//1) for loop
+fun main()
+{
+    for (j in 1..5)
+    {
+        println(j)
+    }
+    var roit = mutableListOf("rohit","kohli","hardik")
+    for(ro in roit)
+    {
+        println(ro)
+    }
+
+   // 2)while loop
+   var i=0
+    while (i<=5)
+    {
+     println(i)
+        i++
+    }
+
+}
